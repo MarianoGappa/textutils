78bacdd449752a685832fc39444de0889d6efdef
Mariano Gappa <mariano@movio.co>
2016-03-24 23:29:28 +1300

    Standalone freq and diff functions.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func readLines(path string) (map[string]int, error) {
@@ -14,12 +15,49 @@ func readLines(path string) (map[string]int, error) {
 	}
 	defer file.Close()
 
-	lines := make(map[string]int)
+	words := make(map[string]int)
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		lines[scanner.Text()]++
+		line := scanner.Text()
+		lineWords := strings.Fields(line)
+		for _, v := range lineWords {
+			if filtered := filterChars(v); len(filtered) > 0 {
+				words[filtered]++
+			}
+		}
 	}
-	return lines, scanner.Err()
+	return words, scanner.Err()
+}
+
+func filterChars(s string) string {
+	result := ""
+	for _, c := range s {
+		if ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') ||
+			c == 'á' || c == 'é' || c == 'í' || c == 'ó' || c == 'ú' ||
+			c == 'Á' || c == 'É' || c == 'Í' || c == 'Ó' || c == 'Ú' ||
+			c == 'ñ' || c == 'Ñ' {
+
+			switch {
+			case c >= 'A' && c <= 'Z':
+				c += 'a' - 'A'
+			case c == 'Á':
+				c = 'á'
+			case c == 'É':
+				c = 'é'
+			case c == 'Í':
+				c = 'í'
+			case c == 'Ó':
+				c = 'ó'
+			case c == 'Ú':
+				c = 'ú'
+			case c == 'Ñ':
+				c = 'ñ'
+			}
+
+			result = result + string(c)
+		}
+	}
+	return result
 }
 
 func diff(lines1 map[string]int, lines2 map[string]int) {
@@ -35,10 +73,17 @@ func main() {
 	flag.Parse()
 	args := flag.Args()
 
-	if args[0] == "diff" {
+	switch args[0] {
+	case "freq":
+		lines, _ := readLines(args[1])
+		for k, v := range lines {
+			fmt.Println(k, v)
+		}
+	case "diff":
 		lines1, _ := readLines(args[1])
 		lines2, _ := readLines(args[2])
 
 		diff(lines1, lines2)
 	}
+
 }
